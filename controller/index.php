bbf5141a579edc0e6df9923ce7839819388cf079
besirep <edzelmundo02@gmail.com>
2025-08-01 14:14:09 +0800

    minor changes to whitespaces(syntax)

--- controller/index.php
+++ controller/index.php
@@ -1,5 +1,5 @@
 <?php
 
-$heading = 'Home'; 
- 
+$heading = 'Homepage';
+
 require 'views/index.view.php';
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,7 +8,7 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/"  class="<?= urlIs('/') || urlIs('/index.php') || urlIs('/index') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+              <a href="/index"  class="<?= urlIs('/index.php') || urlIs('/index') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
               <a href="/about" class="<?= urlIs('/about') || urlIs('/about.php')  ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium">About</a>
               <a href="/contacts" class="<?= urlIs('/contacts') || urlIs('/contacts.php') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> rounded-md px-3 py-2 text-sm font-medium">Contact</a>
             </div>
